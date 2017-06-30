225648585a4ee5f0d9ed2e6063476f05c6d436f5
Vinod Ronold <vinodronoldbe@gmail.com>
2017-06-30 16:54:01 -0400

    Init Preact

---
+++ src/app/components/PlayerLayout/PlayerLayout.js
@@ -0,0 +1,15 @@
+import './PlayerLayout.scss'
+
+const PlayerLayout = () =>
+  <div class={'chords_container'}>
+    <div class={'chords pulse'}>A</div>
+    <div class={'chords active'}>A</div>
+    <div class={'chords'}>A</div>
+    <div class={'chords'}>A</div>
+    <div class={'chords'}>A</div>
+    <div class={'chords'}>A</div>
+    <div class={'chords'}>A</div>
+    <div class={'chords'}>A</div>
+  </div>
+
+export default PlayerLayout
---
+++ src/app/index.js
@@ -0,0 +1,9 @@
+import PlayerLayout from './components/PlayerLayout'
+import './style'
+
+const App = () =>
+  <div>
+    <PlayerLayout />
+  </div>
+
+export default App
